d2d9a73c3fc7ee9c377ae046b3b991ccddaba38b
Paul Norris <paul@pjnorris.com>
2020-07-18 10:10:29 +0100

    Initial Commit

---
+++ Tasker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tasker
+//
+//  Created by Paul Norris on 18/07/2020.
+//  Copyright © 2020 Paul Norris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
